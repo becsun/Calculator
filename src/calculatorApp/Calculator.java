8b48e2bdf4c9b50b78ba19f236322b0d01b4fb19
bec <becsun@icloud.com>
2021-09-03 22:16:45 +0100

    Added a choice of operations

--- src/calculatorApp/Calculator.java
+++ src/calculatorApp/Calculator.java
@@ -1,8 +1,31 @@
 package calculatorApp;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Calculator {
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to the calculator!");
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        int randomNumberTwo = random.nextInt(100);
+
+
+        System.out.println("Random number one:" + randomNumber + "\n" +
+                            "Random number two: "+randomNumberTwo);
+
+        System.out.println( "Enter an operation to perform: + (add), * (multiply) or - (subtract).");
+
+        Scanner scanner = new Scanner(System.in); String input = scanner.next();
+
+        if (input.equals("+")){
+            System.out.println(randomNumber + randomNumberTwo);
+        }else if  (input.equals("-")){
+            System.out.println(randomNumber - randomNumberTwo);
+        }else if  (input.equals("*")) {
+            System.out.println(randomNumber * randomNumberTwo);
+        }
     }
 }
